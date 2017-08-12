963f288ba32fe8c20aa42b41b8c9396d6e82b31e
jkulubya <jameskitamirike@gmail.com>
2017-08-12 10:37:00 +0200

    Make fields on create question vm required

--- server/Models/QuestionViewModels/CreateQuestionViewModel.cs
+++ server/Models/QuestionViewModels/CreateQuestionViewModel.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace homework_helper_server.Models.QuestionViewModels
 {
     public class CreateQuestionViewModel
     {
+        [Required]
         public string Title { get; set; }
+        
+        [Required]
         public string Description { get; set; }
+        
+        [Required]
+        public int CategoryId { get; set; }
     }
 }
